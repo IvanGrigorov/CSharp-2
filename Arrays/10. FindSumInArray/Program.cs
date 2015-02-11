a65be0192b3f40636113905cf83c4e6b695ef31f
Ivan Grigorov <IvanGrigorov@users.noreply.github.com>
2015-02-11 20:13:13 +0200

    Adding Arrays and QuickSort needs some work

---
+++ Arrays/10. FindSumInArray/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Write a program that finds in given array of integers a sequence of given sum S (if present).*/
+
+
+
+using System;
+
+
+namespace _10.FindSumInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array. ");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter what kind of sum you are searching for. ");
+            int S = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[length];
+            int sum = 0;
+            int startIndex = -1;
+            int finalIndeks = -1;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("arr1[{0}] = ", i);
+                arr1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < length && sum != S; i++)
+            {
+                sum = 0;
+                for (int j = i; j < length; j++)
+                {
+                    sum += arr1[j];
+                    if (sum == S)
+                    {
+                        startIndex = i;
+                        finalIndeks = j;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine();
+            if (sum == S)
+            {
+                for (int i = startIndex; i <= finalIndeks; i++)
+                {
+                    Console.Write("{0}, ", arr1[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no such sequence in the array. ");
+            }
+        }
+    }
+}
---
+++ Arrays/14. QuickSort/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Write a program that sorts an array of integers using the Quick sort algorithm.*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14.QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the list. ");
+            int length = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Enter an element (integer) of the list. ");
+                int number = int.Parse(Console.ReadLine());
+                list.Add(number);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("{0}, ",(QuickSort(list))[i]);
+            }
+               
+        }
+
+
+        public static List<int> QuickSort(List<int> array)
+        {
+            int lenght = array.Capacity;
+            if (array.Capacity <= 1)
+            {
+                return array;
+            }
+                int pivot = array[lenght - 1];
+                array.RemoveAt(lenght-1);
+                List<int> less = new List<int>();
+                List<int> greater = new List<int>();
+                for (int i = 0; i < lenght - 1; i++)
+                {
+                    if (array[i] <= pivot)
+                    {
+                        less.Add(array[i]);
+
+                    }
+                    else greater.Add(array[i]);
+                }
+                
+            List<int> newList = new List<int>();
+            (QuickSort(less)).ForEach(p => newList.Add(p));
+            newList.Add(pivot);
+            (QuickSort(greater)).ForEach(p => newList.Add(p));
+            return newList;
+        }
+    }
+}
